a4c2adde18dd073ac19d6b83829eaacc122a26ee
Wersatil <159185890+Wersatil@users.noreply.github.com>
2025-12-29 00:10:23 +0300

    Add files via upload

---
+++ src/test/java/most_valuable_middle/game_service/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package most_valuable_middle.game_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
